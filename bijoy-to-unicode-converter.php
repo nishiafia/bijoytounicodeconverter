570cf3d113f79d24d7905baace50c8a13d7ef92a
Mayeenul Islam <wz.islam@gmail.com>
2014-02-07 00:29:54 +0600

    plugin name changed - minor

--- bijoy-to-unicode-converter.php
+++ bijoy-to-unicode-converter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WordPress Bijoy to Unicode Converter
+ * Plugin Name: Bijoy to Unicode Converter - WordPress 
  * Plugin URI: http://nanodesignsbd.com
  * Description: A one-click ASCII to Unicode converter that can convert Bijoy (ASCII) texts into Unicode Bangla. It can covert Bijoy ASCII texts into Unicode with a single click in WordPress post or page editor - in Text mode. The basic of the plugin was done by <a href="https://github.com/nishiafia">Ms. Tahmina Aktar</a> on the Javascripts of Online Unicode Converter by <a href="http://bnwebtools.sourceforge.net/">S. M. Mahbub Morshed</a>. Then it's verified, modified, simplified, and made translation-ready by <a href="https://github.com/mayeenulislam">Mayeenul Islam</a> from <a href="http://nanodesignsbd.com">nanodesigns</a>.
  * Version: 1.0
